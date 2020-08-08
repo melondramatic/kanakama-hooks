edf5171093d8b44bfefa6b07f78d41429cb1829f
melon <andrew.chin.qq@gmail.com>
2020-08-07 20:30:45 -0400

    Updates

---
+++ precache-manifest.06e9f4b0ede05b561c62346e96a17f17.js
@@ -0,0 +1,382 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d371acd731e2f5968d21bf7fd5bda0e3",
+    "url": "/kanakama-hooks/index.html"
+  },
+  {
+    "revision": "037e813a680b86fc4206",
+    "url": "/kanakama-hooks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f22ca8ffcc444da6dcbb",
+    "url": "/kanakama-hooks/static/js/2.63c458a5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/kanakama-hooks/static/js/2.63c458a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037e813a680b86fc4206",
+    "url": "/kanakama-hooks/static/js/main.386b3965.chunk.js"
+  },
+  {
+    "revision": "9148e5df9587b78773e8",
+    "url": "/kanakama-hooks/static/js/runtime-main.c85cd267.js"
+  },
+  {
+    "revision": "cacb2328e6aeb22e42650cfa803bb01b",
+    "url": "/kanakama-hooks/static/media/hiragana-a.cacb2328.svg"
+  },
+  {
+    "revision": "b04373e1759f1cc89fdefccd77420717",
+    "url": "/kanakama-hooks/static/media/hiragana-chi.b04373e1.svg"
+  },
+  {
+    "revision": "7ae384720b477c4c0575a4b08a84b916",
+    "url": "/kanakama-hooks/static/media/hiragana-fu.7ae38472.svg"
+  },
+  {
+    "revision": "b832da985f7c6e445b95f44eb4723b63",
+    "url": "/kanakama-hooks/static/media/hiragana-he.b832da98.svg"
+  },
+  {
+    "revision": "cc9993fd0909738030d0b9f67e9cde9d",
+    "url": "/kanakama-hooks/static/media/hiragana-hi.cc9993fd.svg"
+  },
+  {
+    "revision": "eb8f569603e2b5c5caa4fd8d5f39361d",
+    "url": "/kanakama-hooks/static/media/hiragana-ho.eb8f5696.svg"
+  },
+  {
+    "revision": "4d6a9b02147c2e5de783a2c45ca6b65b",
+    "url": "/kanakama-hooks/static/media/hiragana-i.4d6a9b02.svg"
+  },
+  {
+    "revision": "02303c41b249ab41087b77b85ea0aa05",
+    "url": "/kanakama-hooks/static/media/hiragana-ka.02303c41.svg"
+  },
+  {
+    "revision": "016749276899eec111d954a47c9ae218",
+    "url": "/kanakama-hooks/static/media/hiragana-ke.01674927.svg"
+  },
+  {
+    "revision": "89bddc91da632a051e4a5b1a9f8a809b",
+    "url": "/kanakama-hooks/static/media/hiragana-ki.89bddc91.svg"
+  },
+  {
+    "revision": "f681766210ad3e2bb14c1a8de5a9f211",
+    "url": "/kanakama-hooks/static/media/hiragana-ko.f6817662.svg"
+  },
+  {
+    "revision": "a647b6cc8f89131d9b1305ea0c5ffd6b",
+    "url": "/kanakama-hooks/static/media/hiragana-ku.a647b6cc.svg"
+  },
+  {
+    "revision": "d9c3048e2c2119fabb675f2f7db02fd6",
+    "url": "/kanakama-hooks/static/media/hiragana-ma.d9c3048e.svg"
+  },
+  {
+    "revision": "1d872f6780148f7b61ae7799430e4904",
+    "url": "/kanakama-hooks/static/media/hiragana-me.1d872f67.svg"
+  },
+  {
+    "revision": "099eeabeaa28460abd668b3c94c089dc",
+    "url": "/kanakama-hooks/static/media/hiragana-mi.099eeabe.svg"
+  },
+  {
+    "revision": "1bfbbb45090b7d7dec6cbe999d525f2e",
+    "url": "/kanakama-hooks/static/media/hiragana-mo.1bfbbb45.svg"
+  },
+  {
+    "revision": "1c2c94fecc0e38552480a5c82cb1957d",
+    "url": "/kanakama-hooks/static/media/hiragana-mu.1c2c94fe.svg"
+  },
+  {
+    "revision": "aeaff89ae821ab47f191c460ad53d7c8",
+    "url": "/kanakama-hooks/static/media/hiragana-n.aeaff89a.svg"
+  },
+  {
+    "revision": "c38a5d0ea344d0e4b961b1b302c0564b",
+    "url": "/kanakama-hooks/static/media/hiragana-na.c38a5d0e.svg"
+  },
+  {
+    "revision": "18701f5d5f28610557c970c509d9edf3",
+    "url": "/kanakama-hooks/static/media/hiragana-ne.18701f5d.svg"
+  },
+  {
+    "revision": "a955229e3a3a3f57b13e5bb1b03760b9",
+    "url": "/kanakama-hooks/static/media/hiragana-ni.a955229e.svg"
+  },
+  {
+    "revision": "8afe65824d4796b27d6f3433f864f252",
+    "url": "/kanakama-hooks/static/media/hiragana-no.8afe6582.svg"
+  },
+  {
+    "revision": "819498aab641a43f25606c471841a9a6",
+    "url": "/kanakama-hooks/static/media/hiragana-nu.819498aa.svg"
+  },
+  {
+    "revision": "907e0c1c595545160ba6d60cc28b012b",
+    "url": "/kanakama-hooks/static/media/hiragana-o.907e0c1c.svg"
+  },
+  {
+    "revision": "fbbb0fe77ca407abab22f0ea69abccba",
+    "url": "/kanakama-hooks/static/media/hiragana-ra.fbbb0fe7.svg"
+  },
+  {
+    "revision": "e8779af625dcc658e49a0d4586406859",
+    "url": "/kanakama-hooks/static/media/hiragana-re.e8779af6.svg"
+  },
+  {
+    "revision": "c60efbcec8198497aab661855a9577a7",
+    "url": "/kanakama-hooks/static/media/hiragana-ri.c60efbce.svg"
+  },
+  {
+    "revision": "6088f32399bd15427ab282756c66904a",
+    "url": "/kanakama-hooks/static/media/hiragana-ro.6088f323.svg"
+  },
+  {
+    "revision": "2844a72ef299d7d040b2058290b442d9",
+    "url": "/kanakama-hooks/static/media/hiragana-ru.2844a72e.svg"
+  },
+  {
+    "revision": "aca02494d9489aae539c3cb014e945c6",
+    "url": "/kanakama-hooks/static/media/hiragana-sa.aca02494.svg"
+  },
+  {
+    "revision": "ff59ba5244cc19528aa93db220f0150d",
+    "url": "/kanakama-hooks/static/media/hiragana-se.ff59ba52.svg"
+  },
+  {
+    "revision": "c83458de3d5f80570008b11faca6bfaf",
+    "url": "/kanakama-hooks/static/media/hiragana-shi.c83458de.svg"
+  },
+  {
+    "revision": "4a1e90feb711ded34d47701ad15ae76f",
+    "url": "/kanakama-hooks/static/media/hiragana-so.4a1e90fe.svg"
+  },
+  {
+    "revision": "df66ec0ecd72087b6de5998f836961f2",
+    "url": "/kanakama-hooks/static/media/hiragana-su.df66ec0e.svg"
+  },
+  {
+    "revision": "9aaa2f68b53357b4d16815b14b542a95",
+    "url": "/kanakama-hooks/static/media/hiragana-ta.9aaa2f68.svg"
+  },
+  {
+    "revision": "7e8cf8bfba3099bfd7053e0994b2c704",
+    "url": "/kanakama-hooks/static/media/hiragana-te.7e8cf8bf.svg"
+  },
+  {
+    "revision": "d1437e921cdb6c3ef751b0ca6a0235c0",
+    "url": "/kanakama-hooks/static/media/hiragana-to.d1437e92.svg"
+  },
+  {
+    "revision": "b959b6ebaa787b82472d5e9a95febc55",
+    "url": "/kanakama-hooks/static/media/hiragana-tsu.b959b6eb.svg"
+  },
+  {
+    "revision": "3e4bc664a8b9ceefcb46172ea6504c2a",
+    "url": "/kanakama-hooks/static/media/hiragana-u.3e4bc664.svg"
+  },
+  {
+    "revision": "f9affc5e50bf8ecbbadb323959103ad9",
+    "url": "/kanakama-hooks/static/media/hiragana-wa.f9affc5e.svg"
+  },
+  {
+    "revision": "d7bb793ac6597db05a1178e98199f52b",
+    "url": "/kanakama-hooks/static/media/hiragana-wo.d7bb793a.svg"
+  },
+  {
+    "revision": "6a44dd00a7b41efbdc67a3389a0085b3",
+    "url": "/kanakama-hooks/static/media/hiragana-ya.6a44dd00.svg"
+  },
+  {
+    "revision": "0f53c70d5f1846832055671155f7c145",
+    "url": "/kanakama-hooks/static/media/hiragana-yu.0f53c70d.svg"
+  },
+  {
+    "revision": "fb3d6712821ae4b4eff6fdbca6ed891b",
+    "url": "/kanakama-hooks/static/media/katakana-a.fb3d6712.svg"
+  },
+  {
+    "revision": "b4644e01a613bf04b19c48f1e267bb59",
+    "url": "/kanakama-hooks/static/media/katakana-chi.b4644e01.png"
+  },
+  {
+    "revision": "e383bef7b1a00d2e3dd3047f97131469",
+    "url": "/kanakama-hooks/static/media/katakana-e.e383bef7.svg"
+  },
+  {
+    "revision": "b156c7bc1ccacee823721af469bbe986",
+    "url": "/kanakama-hooks/static/media/katakana-fu.b156c7bc.png"
+  },
+  {
+    "revision": "700de3d7232ed58413850c06eed36160",
+    "url": "/kanakama-hooks/static/media/katakana-ha.700de3d7.png"
+  },
+  {
+    "revision": "bcb220c771f8020b6bc0ad412ef7b5f8",
+    "url": "/kanakama-hooks/static/media/katakana-he.bcb220c7.png"
+  },
+  {
+    "revision": "fd60b17bb7430213a88824209882cd8e",
+    "url": "/kanakama-hooks/static/media/katakana-hi.fd60b17b.png"
+  },
+  {
+    "revision": "2d0e0cf6a490524881f765842dc05f46",
+    "url": "/kanakama-hooks/static/media/katakana-ho.2d0e0cf6.png"
+  },
+  {
+    "revision": "d512f1a4d9c0cda204232feb5c5e4406",
+    "url": "/kanakama-hooks/static/media/katakana-i.d512f1a4.svg"
+  },
+  {
+    "revision": "73c84f8643b26d1cdc4f29688d6f0f73",
+    "url": "/kanakama-hooks/static/media/katakana-ka.73c84f86.svg"
+  },
+  {
+    "revision": "2b3f9dd0349ba149197ae6a65e1f1a17",
+    "url": "/kanakama-hooks/static/media/katakana-ke.2b3f9dd0.png"
+  },
+  {
+    "revision": "a60d6017954abc879ff0edf031749d8e",
+    "url": "/kanakama-hooks/static/media/katakana-ki.a60d6017.svg"
+  },
+  {
+    "revision": "360958af135e06a13cfb615c28ba7493",
+    "url": "/kanakama-hooks/static/media/katakana-ko.360958af.png"
+  },
+  {
+    "revision": "b07f6f2d3582e9b206e2f108ccac2082",
+    "url": "/kanakama-hooks/static/media/katakana-ku.b07f6f2d.png"
+  },
+  {
+    "revision": "db7483cd7055e3c3502b37ea5749b1f9",
+    "url": "/kanakama-hooks/static/media/katakana-ma.db7483cd.png"
+  },
+  {
+    "revision": "4dcfcf14d82344eb6c2f35217673da2b",
+    "url": "/kanakama-hooks/static/media/katakana-me.4dcfcf14.png"
+  },
+  {
+    "revision": "1a1eeb113e4654fb24da0f16789ab1a6",
+    "url": "/kanakama-hooks/static/media/katakana-mi.1a1eeb11.png"
+  },
+  {
+    "revision": "e133768c562b03baece3a3b73f15d527",
+    "url": "/kanakama-hooks/static/media/katakana-mo.e133768c.png"
+  },
+  {
+    "revision": "4649657bbef9f372f095a65e5e34bd6d",
+    "url": "/kanakama-hooks/static/media/katakana-mu.4649657b.png"
+  },
+  {
+    "revision": "37f5679b4805ccd7a20a04f04ad4eafd",
+    "url": "/kanakama-hooks/static/media/katakana-n.37f5679b.png"
+  },
+  {
+    "revision": "007d4d029ec129aba00657a93a47f8f1",
+    "url": "/kanakama-hooks/static/media/katakana-na.007d4d02.png"
+  },
+  {
+    "revision": "a00760c6855b7ffdd72307914773e1d4",
+    "url": "/kanakama-hooks/static/media/katakana-ne.a00760c6.png"
+  },
+  {
+    "revision": "3f2c73f20a882c62eb9fc9666d3e985b",
+    "url": "/kanakama-hooks/static/media/katakana-ni.3f2c73f2.png"
+  },
+  {
+    "revision": "4797ea0806404569ecabcef159ebc715",
+    "url": "/kanakama-hooks/static/media/katakana-no.4797ea08.png"
+  },
+  {
+    "revision": "d3c415bd27ece009d85a95f94751d7f4",
+    "url": "/kanakama-hooks/static/media/katakana-nu.d3c415bd.svg"
+  },
+  {
+    "revision": "608e276d465849375b7461be738d6973",
+    "url": "/kanakama-hooks/static/media/katakana-o.608e276d.svg"
+  },
+  {
+    "revision": "140112177f47f8ce9b45c79a1a987b1f",
+    "url": "/kanakama-hooks/static/media/katakana-ra.14011217.png"
+  },
+  {
+    "revision": "45dcbf79ec06183fa5dca42e9e5551a9",
+    "url": "/kanakama-hooks/static/media/katakana-re.45dcbf79.png"
+  },
+  {
+    "revision": "aa9955916d19d4d02a8a9f5009846a4b",
+    "url": "/kanakama-hooks/static/media/katakana-ri.aa995591.png"
+  },
+  {
+    "revision": "f076b5e99f966871e8d9c062e8c7dfa5",
+    "url": "/kanakama-hooks/static/media/katakana-ro.f076b5e9.png"
+  },
+  {
+    "revision": "bf9f1bd9e024cb6939056470eddc8782",
+    "url": "/kanakama-hooks/static/media/katakana-ru.bf9f1bd9.png"
+  },
+  {
+    "revision": "432eca7decaab0c8ea731c7fc093f1ea",
+    "url": "/kanakama-hooks/static/media/katakana-sa.432eca7d.svg"
+  },
+  {
+    "revision": "8fa0fd3dee74b11ea87c92251b2ac121",
+    "url": "/kanakama-hooks/static/media/katakana-se.8fa0fd3d.png"
+  },
+  {
+    "revision": "08a2e7e63f854e1184455754a8e5eaf7",
+    "url": "/kanakama-hooks/static/media/katakana-shi.08a2e7e6.png"
+  },
+  {
+    "revision": "dcc4ddd2b50fd8f2ba1c623533060738",
+    "url": "/kanakama-hooks/static/media/katakana-so.dcc4ddd2.png"
+  },
+  {
+    "revision": "b999bfe1254212cb9a94fd7b746d76ad",
+    "url": "/kanakama-hooks/static/media/katakana-su.b999bfe1.png"
+  },
+  {
+    "revision": "19ec3017d2800455c11abdc504bd5a4e",
+    "url": "/kanakama-hooks/static/media/katakana-ta.19ec3017.png"
+  },
+  {
+    "revision": "1c8d61a3c8cfd556aa4d8cc34146a69c",
+    "url": "/kanakama-hooks/static/media/katakana-te.1c8d61a3.png"
+  },
+  {
+    "revision": "19cf3465be6e2c951161732ef1e5066d",
+    "url": "/kanakama-hooks/static/media/katakana-to.19cf3465.png"
+  },
+  {
+    "revision": "776dca3b47e38baf9d81ec3fd3f6b20c",
+    "url": "/kanakama-hooks/static/media/katakana-tsu.776dca3b.png"
+  },
+  {
+    "revision": "47c9b5710c2395a845214858adcf0026",
+    "url": "/kanakama-hooks/static/media/katakana-u.47c9b571.svg"
+  },
+  {
+    "revision": "02da427b7108806934f6896b01eec770",
+    "url": "/kanakama-hooks/static/media/katakana-wa.02da427b.png"
+  },
+  {
+    "revision": "72eaff6b8cb8ba4318f7d6d9161f9669",
+    "url": "/kanakama-hooks/static/media/katakana-wo.72eaff6b.png"
+  },
+  {
+    "revision": "9e80e71a151e200537c7291bbbfa323b",
+    "url": "/kanakama-hooks/static/media/katakana-ya.9e80e71a.png"
+  },
+  {
+    "revision": "daa6c4d2de7c505490a210b4a7bafd93",
+    "url": "/kanakama-hooks/static/media/katakana-yo.daa6c4d2.png"
+  },
+  {
+    "revision": "a6760bed146243b08bbe7220480021dc",
+    "url": "/kanakama-hooks/static/media/katakana-yu.a6760bed.png"
+  }
+]);
