b51e3db86c9cdfc4ba0cc9c9a4d9f13ddfc4f245
melon <andrew.chin.qq@gmail.com>
2021-01-27 23:48:00 -0500

    remove logrocket

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 
 import Routing from './Routing';
-import LogRocket from 'logrocket';
 
 const Styles = makeStyles({
 	root: {
@@ -12,26 +11,6 @@ const Styles = makeStyles({
 		padding: '12px',
 	},
 });
-
-if (process.env.NODE_ENV === 'production') {
-	LogRocket.init('bhu1ag/kanakama', {
-		dom: {
-			inputSanitizer: true,
-		},
-		network: {
-			requestSanitizer: (request) => {
-				if (request.url.toLowerCase().indexOf('login') !== -1) {
-					request.body = undefined;
-				}
-				return request;
-			},
-			responseSanitizer: (response) => {
-				response.body = undefined;
-				return response;
-			},
-		},
-	});
-}
 
 function App() {
 	const classes = Styles();
